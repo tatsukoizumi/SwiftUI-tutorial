4993b171d9a119b89d85dd1e2bf1e28b0c0cbbdb
tatsukoizumi <koizumitattsuu@gmail.com>
2023-07-09 09:57:04 +0900

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Koizumi Tatsunori on 2023/07/09.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
